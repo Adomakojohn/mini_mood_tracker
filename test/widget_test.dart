1daab8f7fa869407688a702540ac76066f9d2e92
Adomakojohn <johnkwakuadomako6@gmail.com>
2025-05-21 16:21:26 +0000

    add required files

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mini_mood_tracker/main.dart';
+import 'package:mini_mood_tracker/screens/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
